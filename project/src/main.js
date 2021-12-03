86af2b5a7dc0ea3b43a921fdd22b2989ed063f91
EvanLaverdiere <0860347@johnabbottcollege.net>
2021-12-03 09:33:04 -0500

    Created global variables to hold document elements necessary for future operations.

--- project/src/main.js
+++ project/src/main.js
@@ -3,3 +3,14 @@
 class Station{
     //To be filled in once more details are available.
 }
+
+//#region Variables
+
+let userRole = document.getElementById("userRole");
+let startDestination = document.getElementById("startDestination");
+let endDestination = document.getElementById("endDestination");
+let startTime = document.getElementById("startTime");
+
+console.log(userRole);
+
+//#endregion
