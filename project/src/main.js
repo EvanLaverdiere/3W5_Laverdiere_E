6400936e5aedc88a54a00a85995bae2fcbc00bf4
EvanLaverdiere <0860347@johnabbottcollege.net>
2021-12-03 09:37:44 -0500

    Added variable for submit button to main.js.

--- project/src/main.js
+++ project/src/main.js
@@ -10,7 +10,12 @@ let userRole = document.getElementById("userRole");
 let startDestination = document.getElementById("startDestination");
 let endDestination = document.getElementById("endDestination");
 let startTime = document.getElementById("startTime");
+let submitBtn = document.getElementById("submitBtn");
 
 console.log(userRole);
+console.log(startDestination);
+console.log(endDestination);
+console.log(startTime);
+console.log(submitBtn);
 
 //#endregion
